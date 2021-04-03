ebad003af17f517033c3c0be71941d603d592981
Nedim Ajdin <nedim.ajdin@stu.ssst.edu.ba>
2021-04-03 11:01:40 +0200

    Initial commit

---
+++ src/main/java/ba/edu/ssst/ptuiserver/PtUiserverApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ssst.ptuiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtUiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtUiserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/edu/ssst/ptuiserver/PtUiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ssst.ptuiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtUiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
