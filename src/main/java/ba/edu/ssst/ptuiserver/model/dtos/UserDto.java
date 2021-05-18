f6a1feb30bf3baa0445e444042ea0f792c81d49e
Nedim Ajdin <nedim.ajdin@stu.ssst.edu.ba>
2021-05-18 22:51:00 +0200

    fixed CORS for netlify

--- src/main/java/ba/edu/ssst/ptuiserver/model/dtos/UserDto.java
+++ src/main/java/ba/edu/ssst/ptuiserver/model/dtos/UserDto.java
@@ -1,8 +1,6 @@
 package ba.edu.ssst.ptuiserver.model.dtos;
 
-import ba.edu.ssst.ptuiserver.model.entities.Location;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +13,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UserDto extends GenericDto{
+public class GetSingleUserDTO extends GenericDto{
     private Long id;
     private String firstName;
     private String lastName;
@@ -26,8 +24,5 @@ public class UserDto extends GenericDto{
     @JsonFormat(pattern="dd/MM/yyyy")
     private Date dob;
     private String jmbg;
-    @JsonIgnore
-    private Location location;
     private LocationDto locationDto;
-    private long locationId;
 }
